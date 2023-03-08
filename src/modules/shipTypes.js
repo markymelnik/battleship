67d9ebaf27b41a26236c4cb7ff484cb5ce1938dd
markymelnik <markemelnik@gmail.com>
2023-03-07 21:57:06 -0500

    Initial modules

---
+++ src/modules/shipTypes.js
@@ -0,0 +1,29 @@
+const shipTypes = {
+  destroyer: {
+    id: 1,
+    name: 'destroyer',
+    length: 2
+  },
+  submarine: {
+    id: 2,
+    name: 'submarine',
+    length: 3
+  },
+  cruiser: {
+    id: 3,
+    name: 'cruiser',
+    length: 3
+  },
+  battleship: {
+    id: 4,
+    name: 'battleship',
+    length: 4
+  },
+  carrier: {
+    id: 5,
+    name: 'carrer',
+    length: 5
+  }
+}
+
+export default shipTypes;

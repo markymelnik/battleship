f920f93b574947a1e977f804f0885bb55a3e027d
markymelnik <markemelnik@gmail.com>
2023-03-08 14:55:33 -0500

    Updated tests for ship module

---
+++ src/modules/tests/ship.test.js
@@ -0,0 +1,30 @@
+const { Ship } = require('../ship');
+
+let cruiser; 
+
+beforeEach(() => {
+  cruiser = Ship('cruiser');
+});
+
+test('accurate information about cruiser', () => {
+  expect(cruiser.type).toBe('cruiser');
+  expect(cruiser.length).toBe(3);
+  expect(cruiser.hits).toBe(0);
+});
+
+test('cruiser is hit 2 times', () => {
+  expect(cruiser.hits).toBe(0);
+  cruiser.hit();
+  expect(cruiser.hits).toBe(1);
+  cruiser.hit();
+  expect(cruiser.hits).toBe(2);
+})
+
+test('cruiser is sunk after 3 hits', () => {
+  expect(cruiser.isSunk()).toBe(false);
+  cruiser.hit();
+  cruiser.hit();
+  expect(cruiser.isSunk()).toBe(false);
+  cruiser.hit(); 
+  expect(cruiser.isSunk()).toBe(true);
+});
